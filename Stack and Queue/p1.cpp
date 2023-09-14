1b762be4d33568ff46a93fbcc4d97d40295d7510
Mohd Haider <86488954+mohdhaider07@users.noreply.github.com>
2023-09-14 11:41:41 +0000

    next greater element done

--- Stack and Queue/p1.cpp
+++ Stack and Queue/p1.cpp
@@ -1,10 +1,52 @@
 #include<iostream>
+#include<vector>
+#include<stack>
 using namespace std;
 
 
 
-int main(){
 
+vector<int> getNextLargest(vector<int>arr){
+    if(arr.size()==1){
+        return {-1};
+    }
+
+    vector<int> ans;
+    stack<int>s;
+    int n=arr.size();
+
+    for(int i=n-1;i>=0;i--){
+        if(s.empty()){
+            ans.insert(ans.begin(),-1);
+            s.push(arr[i]);
+        }else if (!s.empty() && arr[i]>s.top()){
+            while(!s.empty() && arr[i]>s.top()){
+                s.pop();
+            }
+            if(s.empty()){
+                 ans.insert(ans.begin(),-1);
+                s.push(arr[i]);
+            }else{
+                ans.insert(ans.begin(),s.top());
+                s.push(arr[i]);
+            }
+        }else {
+             ans.insert(ans.begin(),s.top());
+                s.push(arr[i]);
+        }
+    }
+    return ans;
+}
+
+
+
+int main(){
+        vector<int>v{6 ,8, 0, 1, 3};
+        v=getNextLargest(v);
+
+        for(auto i:v){
+            cout<<i<<" ";
+        }
 
 
     cout<<"\nMohd Haider"<<endl;
