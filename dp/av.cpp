c5d20d77ecfb58a4dc0b4bce51b9c65776635a62
Mohd Haider <86488954+mohdhaider07@users.noreply.github.com>
2023-09-18 19:07:06 +0000

    some changes

--- dp/av.cpp
+++ dp/av.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+
+// 0 1 knapsack prpblem 
+
+int knapsack(vector<int>profit,vector<int>wight,int capacity,int i){
+    if(i>profit.size()|| capacity<=0){
+        return 0;
+    }
+
+    if(wight[i]>capacity){
+        return knapsack(profit,wight,capacity,i+1);
+    }else{
+        return max( 
+            knapsack(profit,wight,capacity - wight[i],i+1) + profit[i], knapsack(profit,wight,capacity,i+1)
+        )
+    }
+}
 
 int main(){
 
+    vector<int>w{4,5,1};
+    vector<int>p{1,2,3};
+  cout<<knapsack(p,w,4,0);
     cout<<"\n Code executed";
     return 0;
 }
