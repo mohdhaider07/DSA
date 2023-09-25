0ade3f55049d9849cba4788492ded7808664660c
Mohd Haider <86488954+mohdhaider07@users.noreply.github.com>
2023-09-25 12:44:46 +0530

    some chnages

--- dp/todo.cpp
+++ dp/todo.cpp
@@ -1,3 +1,6 @@
 // https://leetcode.com/problems/partition-array-into-two-arrays-to-minimize-sum-difference/description/
 
 
+// unbounded knapsack
+// https://leetcode.com/problems/minimum-cost-to-cut-a-stick/description/
+
