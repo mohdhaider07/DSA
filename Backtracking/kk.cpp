b4a66a7b553c3a66964de130898709436625405f
haider <haiderahmed12786@gmail.com>
2023-10-25 13:41:21 +0530

    some changes

--- Backtracking/kk.cpp
+++ Backtracking/kk.cpp
@@ -91,45 +91,51 @@ void printPathString(int i, int j, int n, string path)
     }
 }
 
-void nearestExitHelper(vector<vector<char>> &maze, int i, int j, int tempAns, int &ans)
+void nearestExitHelper(vector<vector<char>> &maze, int i, int j, int tempAns, int &ans, vector<int> &entrance)
 {
 
     int n = maze.size();
     int m = maze[0].size();
-
-    if (i > 0 || i >= n || j < 0 || j >= m)
+    // cout << " I am here " << i << j << endl;
+    if (i < 0 || i >= n || j < 0 || j >= m || maze[i][j] == '+')
     {
+        cout << " returning :" << endl;
         return;
     }
 
-    if (i == 0 || j == 0 || i == n - 1 || j == m - 1)
+    if (!(entrance[0] == i && entrance[1] == j) && (i == 0 || j == 0 || i == n - 1 || j == m - 1))
     {
-        cout << " 105 ";
+        // cout << " assigning min value " << endl;
         ans = min(ans, tempAns);
         return;
     }
 
     maze[i][j] = '+';
     // up i-1,j
-    if (i - 1 > 0 && maze[i - 1][j] != '+')
+
+    if (i - 1 >= 0 && maze[i - 1][j] != '+')
     {
-        cout << " 110 ";
-        nearestExitHelper(maze, i - 1, j, tempAns + 1, ans);
+
+        cout << " up " << endl;
+        nearestExitHelper(maze, i - 1, j, tempAns + 1, ans, entrance);
     }
     // down i+1,j
-    if (i + 1 < n - 1 && maze[i + 1][j] != '+')
+    if (i + 1 < n && maze[i + 1][j] != '+')
     {
-        nearestExitHelper(maze, i + 1, j, tempAns + 1, ans);
+        cout << " down " << endl;
+        nearestExitHelper(maze, i + 1, j, tempAns + 1, ans, entrance);
     }
     // left i,j-1
-    if (j - 1 > 0 && maze[i][j - 1] != '+')
+    if (j - 1 >= 0 && maze[i][j - 1] != '+')
     {
-        nearestExitHelper(maze, i, j - 1, tempAns + 1, ans);
+        cout << "left" << endl;
+        nearestExitHelper(maze, i, j - 1, tempAns + 1, ans, entrance);
     }
     // right i,j+1
-    if (j + 1 < m - 1 && maze[i][j + 1] != '+')
+    if (j + 1 < m && maze[i][j + 1] != '+')
     {
-        nearestExitHelper(maze, i, j + 1, tempAns + 1, ans);
+        cout << "right" << endl;
+        nearestExitHelper(maze, i, j + 1, tempAns + 1, ans, entrance);
     }
 }
 
@@ -137,23 +143,26 @@ int nearestExit(vector<vector<char>> maze, vector<int> entrance)
 {
 
     int ans = INT_MAX;
-    nearestExitHelper(maze, entrance[0], entrance[1], 0, ans);
+    nearestExitHelper(maze, entrance[0], entrance[1], 0, ans, entrance);
     return ans == INT_MAX ? -1 : ans;
 }
+
 int main()
 {
 
-    int n = 3;
-    vector<vector<int>> matrix(n, vector<int>(n, 0));
+    // int n = 3;
+    // vector<vector<int>> matrix(n, vector<int>(n, 0));
 
     // cout<<"Number of ways : "<<coutTheway(matrix,0,0,n);
 
     // printThePath(matrix,0,0,n);
 
     // printPathString(0, 0, n, "");
-    vector<vector<char>> maze = {{'+', '+', '.', '+'}, {'.', '.', '.', '+'}, {'+', '+', '+', '.'}};
 
-    cout << "Minimum steps is : " << nearestExit(maze, {1, 2});
+    // vector<vector<char>> maze = {{'.', '+'}};
+    vector<vector<char>> maze = {{'+', '+', '+'}, {'.', '.', '.'}, {'+', '+', '+'}};
+
+    cout << "Minimum steps is : " << nearestExit(maze, {1, 0});
     cout << "\nBackTracing";
     return 0;
 }
