050dffdec40b5fa941319c76c0e0ed7db21f8b5c
mohdhaider07 <haiderahmed12786@gmail.com>
2024-07-25 12:14:13 +0530

    somehjcange sare done

---
+++ Striver/sde sheet/Greedy/p1.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int maxMeetings(int s[], int e[], int n)
+{
+    vector<pair<int, int>> meet(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        meet[i] = {e[i], s[i]};
+    }
+
+    // Sort meetings based on end time
+    sort(meet.begin(), meet.end());
+
+    int count = 1;
+    int endTime = meet[0].first;
+    for (int i = 1; i < n; i++)
+    {
+        if (endTime < meet[i].second)
+        {
+            endTime = meet[i].first;
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int findPlatform(int arr[], int dep[], int n)
+{
+    // Your code here
+    vector<pair<int, char>> train;
+    for (int i = 0; i < n; i++)
+    {
+        train.push_back({arr[i], 'A'});
+        train.push_back({dep[i], 'D'});
+    }
+
+    sort(train.begin(), train.end());
+
+    int count = 0;
+    int maxCount = 0;
+
+    for (int i = 0; i < train.size(); i++)
+    {
+        pair<int, char> thisTrain = train[i];
+        int time = thisTrain.first;
+        char ad = thisTrain.second;
+
+        if (ad == 'A')
+        {
+            count++;
+            maxCount = max(maxCount, count);
+        }
+        else
+        {
+            count--;
+        }
+    }
+
+    return maxCount;
+}
+
+// (1,4,20),(2,1,10),(3,1,40),(4,1,30)
+struct Job
+{
+    int id;     // Job Id
+    int dead;   // Deadline of job
+    int profit; // Profit if job is over before or on deadline
+};
+
+bool comparator(pair<int, int> &lhs, pair<int, int> &rhs)
+{
+    if (lhs.first < rhs.first)
+    {
+        // Sort in ascending order of deadline
+        return true;
+    }
+    else if (lhs.first == rhs.first)
+    {
+        // If deadlines are the same, sort in descending order of profit
+        return lhs.second > rhs.second;
+    }
+    return false;
+}
+vector<int> JobScheduling(Job arr[], int n)
+{
+    // your code here
+    // vector will store {dealdile , profit}
+    vector<pair<int, int>> jobs;
+    for (int i = 0; i < n; i++)
+    {
+        jobs.push_back({arr[i].dead, arr[i].profit});
+    }
+    // PRINT JOBS VECTOR
+    for (int i = 0; i < jobs.size(); i++)
+    {
+        cout << jobs[i].first << " " << jobs[i].second << endl;
+    }
+    // now i want to sort the vector like the dealdline which is smalest come first if the two jobs have the same deadline then the job which will come first which will have the highest profit
+    sort(jobs.begin(), jobs.end(), comparator);
+    // PRINT JOBS VECTOR AFTER SORTING
+    for (int i = 0; i < jobs.size(); i++)
+    {
+        cout << jobs[i].first << " " << jobs[i].second << endl;
+    }
+    int profitSum = jobs[0].second;
+    int jobCount = 1;
+    int currentTime = jobs[0].first;
+    for (int i = 1; i < jobs.size(); i++)
+    {
+        /* code */
+        if (currentTime < jobs[i].first)
+        {
+            jobCount++;
+            profitSum += jobs[i].second;
+            currentTime = jobs[i].first;
+        }
+    }
+
+    return {jobCount, profitSum};
+}
+
+int main()
+{
+    int s[] = {1, 3, 0, 5, 8, 5};
+    int e[] = {2, 4, 10, 7, 9, 9};
+    int n = sizeof(s) / sizeof(s[0]);
+    cout << maxMeetings(s, e, n);
+
+    cout << "\nGreedy from sde sheet of striver \n";
+
+    return 0;
+}
--- TRIE/p1.cpp
+++ TRIE/p1.cpp
@@ -1,8 +1,50 @@
 // sample code for c++
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <math.h>
 
 using namespace std;
+
+// TODO: 1.  https://takeuforward.org/data-structure/maximum-xor-of-two-numbers-in-an-array/
+//           https : // www.youtube.com/watch?v=EIhAwfHubE8&list=PLgUwDviBIf0pcIDCZnxhv0LkHf5KzG9zp&index=7
+// TODO: 2.  https://takeuforward.org/trie/maximum-xor-queries-trie/
+
+void AllPossibleStringsHelper(string s, int index, string word, vector<string> &ans)
+{
+    // int size of the string
+    int n = s.size();
+    if (index >= n)
+    {
+        ans.push_back(word);
+        return;
+    }
+
+    AllPossibleStringsHelper(s, index + 1, word + s[index], ans);
+    AllPossibleStringsHelper(s, index + 1, word, ans);
+}
+vector<string> AllPossibleStrings(string s)
+{
+    // Code here
+    int n = s.size();
+    vector<string> ans;
+    // iterate to all the power set
+    for (int i = 0; i < pow(2, n) - 1; i++)
+    {
+        // iterate to size for the word for indexing
+        string str = "";
+        for (int j = 0; j < n; j++)
+        {
+            // left shift of 1, j times
+            if (i & (1 << j))
+            {
+                str = str + s[j];
+            }
+        }
+        ans.push_back(str);
+    }
+    return ans;
+}
 
 int main()
 {
