fe0aaafd2e49bc72cbf9a682c2d61369941998ff
mohdhaider07 <haiderahmed12786@gmail.com>
2025-01-04 13:40:55 +0530

    n queen problem solved

--- Striver/sde sheet/Recursion/p1.cpp
+++ Striver/sde sheet/Recursion/p1.cpp
@@ -34,6 +34,43 @@ void permuteHelper(vector<int> &nums,int index,vector<vector<int>>&ans){
         
     }
 
+// N queen problem 
+
+bool isSafe(int row, int col, vector<string> &board,int n){
+    for(int i=0;i<row;i++){
+        if(board[i][col]=='Q')return false;
+    }
+    // check for the upper-left diagonal
+    for(int i=row-1,j=col-1;i>=0&&j>=0;i--, j--){
+        if(board[i][j]=='Q') return false;
+    }
+    for(int i=row-1,j = col+1; i>=0&& j<n;i--,j++){
+        if(board[i][j]=='Q') return false;
+    }
+    return true;
+}
+
+void solve(int row, vector<string>&board,vector<vector<string>>&solutions,int n){
+    // if all queens are placed save the solution
+    if(row==n){
+        solutions.push_back(board);
+        return;
+    }
+    for(int col=0;col<n;col++){
+        if(isSafe(row,col,board,n)){
+            board[row][col]='Q';
+            solve(row+1,board,solutions,n);
+            board[row][col]='.';
+        }
+    }
+}
+
+vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>solutions;
+        vector<string>board(n,string(n,'.'));
+        solve(0,board,solutions,n);
+        return solutions;
+    }
 
 int main()
 {
